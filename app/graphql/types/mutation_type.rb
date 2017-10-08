1353723259b99590520407962c9f83f356c53ea8
geng <snggeng@gmail.com>
2017-10-08 19:59:53 -0400

    add test for create link

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
+  field :createLink, function: Resolvers::CreateLink.new
   # queries are just represented as fields
   field :allLinks, !types[Types::LinkType] do
     # resolve would be called in order to fetch data for that field
