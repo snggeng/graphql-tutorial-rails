640b95cde49bad10ba7ef2a7b0273069806e4a26
geng <snggeng@gmail.com>
2017-10-08 20:44:07 -0400

    update typo: changed ctx to _ctx in resolvers/sign_in_user.rb

--- app/graphql/resolvers/sign_in_user.rb
+++ app/graphql/resolvers/sign_in_user.rb
@@ -25,7 +25,7 @@ class Resolvers::SignInUser < GraphQL::Function
     crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
     token = crypt.encrypt_and_sign("user-id:#{ user.id }")
 
-    ctx[:session][:token] = token
+    _ctx[:session][:token] = token
 
     OpenStruct.new({
       user: user,
