f58c62e4185509cc50c4afd323994db3967ae2bf
geng <snggeng@gmail.com>
2017-10-08 20:59:15 -0400

    added links_search_test

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,4 +8,6 @@ Types::QueryType = GraphQL::ObjectType.define do
     # resolve would be called in order to fetch data for that field
     resolve -> (obj, args, ctx) { Link.all }
   end
+
+  field :allLinks, function: Resolvers::LinksSearch
 end
