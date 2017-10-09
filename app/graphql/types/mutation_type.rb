97d02f3bc77571dffe69379bd7bf7d5aefd60d8c
geng <snggeng@gmail.com>
2017-10-08 20:27:10 -0400

    added create_user mutation to graphql/types/mutation_type.rb

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
   field :createLink, function: Resolvers::CreateLink.new
+  field :createUser, function: Resolvers::CreateUser.new
   # queries are just represented as fields
   field :allLinks, !types[Types::LinkType] do
     # resolve would be called in order to fetch data for that field
