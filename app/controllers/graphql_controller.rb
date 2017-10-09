c01f85feee483d0cac55cf0d390163d7b8cff41c
geng <snggeng@gmail.com>
2017-10-08 20:38:25 -0400

    updated graph_controller.rb

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,35 +1,48 @@
 class GraphqlController < ApplicationController
   def execute
-    variables = ensure_hash(params[:variables])
-    query = params[:query]
-    operation_name = params[:operationName]
-    context = {
-      # we need to provide session and current user
-      session: session,
-      current_user: current_user
-    }
-    result = GraphqlTutorialSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+     variables = ensure_hash(params[:variables])
+     query = params[:query]
+     operation_name = params[:operationName]
+     context = {
+       # we need to provide session and current user
+       session: session,
+       current_user: current_user
+     }
+     result = GraphqlTutorialSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
 
-    render json: result
-  end
+     render json: result
+   end
 
-  private
+   private
 
-  # gets current user from token stored in session
-  def current_user
-    # if we want to change the sign-in strategy, this is the place todo it
-    return unless session[:token]
+   # gets current user from token stored in session
+   def current_user
+     # if we want to change the sign-in strategy, this is the place todo it
+     return unless session[:token]
 
-    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
-    token = crypt.decrypt_and_verify session[:token]
-    user_id = token.gsub('user-id:', '').to_i
-    User.find_by id: user_id
-  rescue ActiveSupport::MessageVerifier::InvalidSignature
-    nil
-  end
-
+     crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
+     token = crypt.decrypt_and_verify session[:token]
+     user_id = token.gsub('user-id:', '').to_i
+     User.find_by id: user_id
+   rescue ActiveSupport::MessageVerifier::InvalidSignature
+     nil
+   end
+  
   # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
-    # ...code
+    case ambiguous_param
+    when String
+      if ambiguous_param.present?
+        ensure_hash(JSON.parse(ambiguous_param))
+      else
+        {}
+      end
+    when Hash, ActionController::Parameters
+      ambiguous_param
+    when nil
+      {}
+    else
+      raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+    end
   end
 end
